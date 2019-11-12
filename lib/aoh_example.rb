1681b489194cb5c6a74aeccf9c9101ea181bc174
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 06:29:47 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,6 +27,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  assembled_aoh[1][:occupation] = new_value# Update the AoH data at row and key to have the value of new_value
-  aoh_update# Return the updated AoH
+  result = assembled_aoh[1][:occupation] = new_value# Update the AoH data at row and key to have the value of new_value
+  result# Return the updated AoH
 end
